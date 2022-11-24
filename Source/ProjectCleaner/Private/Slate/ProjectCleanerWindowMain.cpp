c013420589ab2b19b3fa56f2be09562593e93a3c
Ashot Barkhudaryan <ashot7410@gmail.com>
2022-11-24 04:53:45 +0400

    refactor: stats
    - separate class file for stats list item

--- Source/ProjectCleaner/Private/Slate/ProjectCleanerWindowMain.cpp
+++ Source/ProjectCleaner/Private/Slate/ProjectCleanerWindowMain.cpp
@@ -1,6 +1,7 @@
 ﻿// Copyright Ashot Barkhudaryan. All Rights Reserved.
 
 #include "Slate/ProjectCleanerWindowMain.h"
+#include "Slate/ProjectCleanerStatListItem.h"
 #include "ProjectCleanerTypes.h"
 #include "ProjectCleanerStyles.h"
 #include "ProjectCleanerConstants.h"
@@ -14,48 +15,6 @@
 
 static constexpr int32 WidgetIndexNone = 0;
 static constexpr int32 WidgetIndexLoading = 1;
-
-void SProjectCleanerStatListItem::Construct(const FArguments& InArgs, const TSharedRef<STableViewBase>& InOwnerTableView)
-{
-	ListItem = InArgs._ListItem;
-
-	SMultiColumnTableRow<TWeakObjectPtr<UProjectCleanerStatListItem>>::Construct(
-		SMultiColumnTableRow<TWeakObjectPtr<UProjectCleanerStatListItem>>::FArguments()
-		.Padding(FMargin{0.0f, 2.0f, 0.0f, 0.0f}),
-		InOwnerTableView
-	);
-}
-
-TSharedRef<SWidget> SProjectCleanerStatListItem::GenerateWidgetForColumn(const FName& InColumnName)
-{
-	if (InColumnName.IsEqual(TEXT("Name")))
-	{
-		return
-			SNew(STextBlock)
-				.AutoWrapText(false)
-				.Justification(ETextJustify::Center)
-				.Text(FText::FromString(ListItem->Name))
-				.ColorAndOpacity(FLinearColor{FColor::FromHex(TEXT("#00a6fb"))})
-				.Font(FProjectCleanerStyles::Get().GetFontStyle("ProjectCleaner.Font.Light13"));
-	}
-
-	if (InColumnName.IsEqual(TEXT("Category")))
-	{
-		return SNew(STextBlock).Text(FText::FromString(ListItem->Category)).ColorAndOpacity(ListItem->Color);
-	}
-
-	if (InColumnName.IsEqual(TEXT("Count")))
-	{
-		return SNew(STextBlock).Text(FText::FromString(ListItem->Count)).ColorAndOpacity(ListItem->Color);
-	}
-
-	if (InColumnName.IsEqual(TEXT("Size")))
-	{
-		return SNew(STextBlock).Text(FText::FromString(ListItem->Size)).ColorAndOpacity(ListItem->Color);
-	}
-
-	return SNew(STextBlock).Text(FText::FromString(TEXT("")));
-}
 
 void SProjectCleanerWindowMain::Construct(const FArguments& InArgs)
 {
--- Source/ProjectCleaner/Public/Slate/ProjectCleanerWindowMain.h
+++ Source/ProjectCleaner/Public/Slate/ProjectCleanerWindowMain.h
@@ -8,22 +8,6 @@
 class UProjectCleanerScanSettings;
 class UProjectCleanerStatListItem;
 
-class SProjectCleanerStatListItem final : public SMultiColumnTableRow<TWeakObjectPtr<UProjectCleanerStatListItem>>
-{
-public:
-	SLATE_BEGIN_ARGS(SProjectCleanerStatListItem)
-		{
-		}
-
-		SLATE_ARGUMENT(TWeakObjectPtr<UProjectCleanerStatListItem>, ListItem)
-	SLATE_END_ARGS()
-
-	void Construct(const FArguments& InArgs, const TSharedRef<STableViewBase>& InOwnerTableView);
-	virtual TSharedRef<SWidget> GenerateWidgetForColumn(const FName& InColumnName) override;
-private:
-	TWeakObjectPtr<UProjectCleanerStatListItem> ListItem;
-};
-
 class SProjectCleanerWindowMain final : public SCompoundWidget
 {
 public:
