2b6140d6f585df2d1352bc6fa193fd81b78c7aa3
Ashot Barkhudaryan <ashot7410@gmail.com>
2022-12-28 02:44:53 +0400

    feat: lib path
    - added new blueprint exposed class for working with paths

---
+++ Source/ProjectCleaner/Private/Libs/ProjectCleanerLibPath.cpp
@@ -0,0 +1,64 @@
+﻿// Copyright Ashot Barkhudaryan. All Rights Reserved.
+
+#include "Libs/ProjectCleanerLibPath.h"
+#include "ProjectCleanerConstants.h"
+
+
+FString UProjectCleanerLibPath::Normalize(const FString& InPath)
+{
+	if (InPath.IsEmpty()) return InPath;
+	
+	FString Path = FPaths::ConvertRelativePathToFull(InPath);
+	FPaths::RemoveDuplicateSlashes(Path);
+
+	if (FPaths::GetExtension(Path).IsEmpty())
+	{
+		FPaths::NormalizeDirectoryName(Path);
+	}
+	else
+	{
+		FPaths::NormalizeFilename(Path);
+	}
+
+	return Path;
+}
+
+FString UProjectCleanerLibPath::ConvertToAbs(const FString& InPath)
+{
+	const FString PathNormalized = Normalize(InPath);
+
+	if (PathIsUnderContentFolder(PathNormalized)) return {};
+
+	return PathNormalized.Replace(
+		*ProjectCleanerConstants::PathRelRoot.ToString(),
+		*FPaths::ConvertRelativePathToFull(FPaths::ProjectDir() / TEXT("Content")),
+		ESearchCase::CaseSensitive
+	);
+}
+
+FString UProjectCleanerLibPath::ConvertToRel(const FString& InPath)
+{
+	const FString PathNormalized = Normalize(InPath);
+
+	if (PathIsUnderContentFolder(PathNormalized)) return {};
+
+	return PathNormalized.Replace(
+		*FPaths::ConvertRelativePathToFull(FPaths::ProjectDir() / TEXT("Content")),
+		*ProjectCleanerConstants::PathRelRoot.ToString(),
+		ESearchCase::CaseSensitive
+	);
+}
+
+bool UProjectCleanerLibPath::PathIsUnderContentFolder(const FString& InPath)
+{
+	const FString PathNormalized = Normalize(InPath);
+
+	if (
+		!InPath.StartsWith(ProjectCleanerConstants::PathRelRoot.ToString()) &&
+		!InPath.StartsWith(FPaths::ConvertRelativePathToFull(FPaths::ProjectDir() / TEXT("Content"))))
+	{
+		return false;
+	}
+
+	return true;
+}
---
+++ Source/ProjectCleaner/Public/Libs/ProjectCleanerLibPath.h
@@ -0,0 +1,25 @@
+﻿// Copyright Ashot Barkhudaryan. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "ProjectCleanerLibPath.generated.h"
+
+UCLASS()
+class UProjectCleanerLibPath final : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="ProjectCleaner|Lib|Path", meta=(ToolTip="Normalized given file or directory path."))
+	static FString Normalize(const FString& InPath);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="ProjectCleaner|Lib|Path", meta=(ToolTip="Converts given to absolute path. If path is outside Content folder will return empty string."))
+	static FString ConvertToAbs(const FString& InPath);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="ProjectCleaner|Lib|Path", meta=(ToolTip="Converts given to relative path. If path is outside Content folder will return empty string."))
+	static FString ConvertToRel(const FString& InPath);
+private:
+	static bool PathIsUnderContentFolder(const FString& InPath);
+};
