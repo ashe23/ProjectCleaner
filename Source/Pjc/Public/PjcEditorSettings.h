edbfbda96a4fde6170f28c3028d42b4c7818d451
Ashot Barkhudaryan <ashot7410@gmail.com>
2023-04-10 19:37:38 +0400

    feat: plugin editor settings
    - added class for editor settings
    - can be exported/imported for large set of settings

---
+++ Source/Pjc/Public/PjcEditorSettings.h
@@ -0,0 +1,37 @@
+﻿// Copyright Ashot Barkhudaryan. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PjcTypes.h"
+#include "PjcEditorSettings.generated.h"
+
+UCLASS(Config = EditorPerProjectUserSettings)
+class UPjcEditorSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+
+public:
+	virtual FName GetContainerName() const override;
+	virtual FName GetCategoryName() const override;
+	virtual FName GetSectionName() const override;
+	virtual FText GetSectionText() const override;
+	virtual FText GetSectionDescription() const override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Config, Category="General")
+	EPjcCleanupMethod CleanupMethod = EPjcCleanupMethod::Full;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Config, Category="ExcludeSettings", meta=(ContentDir, ToolTip="Consider assets in specified paths as used. Recursive."))
+	TArray<FDirectoryPath> ExcludedPaths;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Config, Category="ExcludeSettings", meta=(ContentDir, ToolTip="Consider assets of specified classes as used."))
+	TArray<TSoftClassPtr<UObject>> ExcludedClasses;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Config, Category="ExcludeSettings", meta=(ContentDir, ToolTip="Consider specified assets as used."))
+	TArray<TSoftObjectPtr<UObject>> ExcludedAssets;
+
+protected:
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+};
